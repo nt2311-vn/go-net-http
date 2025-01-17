5380fb89fa069b542f764e7c2ade5028c076897c
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-17 21:45:10 +0700

    create mux for routes and serve with interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
-type server struct {
+type api struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write([]byte("index page"))
-			return
-		case "/users":
-			w.Write([]byte("users page"))
-			return
-		default:
-			w.Write([]byte("404 page not found"))
-			return
-		}
-	}
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Users list..."))
+}
+
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Created user"))
 }
 
 func main() {
-	s := &server{addr: ":8080"}
-	if err := http.ListenAndServe(s.addr, s); err != nil {
-		log.Fatal(err)
-	}
+	api := &api{addr: ":8080"}
+
+	// Initialize a ServeMux
+	mux := http.NewServeMux()
+
+	srv := &http.Server{Addr: api.addr, Handler: mux}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+
+	srv.ListenAndServe()
 }
