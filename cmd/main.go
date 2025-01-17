dcf63493f3a86d223f7e39c460923ceba400a7e1
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-17 21:34:24 +0700

    simple router with standard library package

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("index page"))
+			return
+		case "/users":
+			w.Write([]byte("users page"))
+			return
+		default:
+			w.Write([]byte("404 page not found"))
+			return
+		}
+	}
+}
+
 func main() {
+	s := &server{addr: ":8080"}
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
+	}
 }
