72be55c438ecfba088c028a4fb6c63361265feea
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-18 09:06:49 +0700

    api complete feature on get and create user

--- cmd/api.go
+++ cmd/api.go
@@ -17,11 +17,21 @@ func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.NewEncoder(w).Encode(users)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 }
 
 func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Created user"))
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	u := User{FirstName: user.FirstName, LastName: user.LastName}
+	users = append(users, u)
+	w.WriteHeader(http.StatusCreated)
 }
